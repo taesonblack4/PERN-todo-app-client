ab9b991e25cbb8509f0ba654d09eeedc118556b4
taeblack4 <taesonblack4@gmail.com>
2024-08-26 12:05:05 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,68 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const HOST = 'http://localhost:3000/todos'
+  const HOST = 'http://localhost:3000/todos/'
   const [state, setState] = useState([]);
   const [input, setInput] = useState('');
 
-  useEffect(() => {
-    const fetchTodos = async () => {
+  //return list of todos sorted 
+  const fetchTodos = async () => {
     const response = await axios.get(HOST);
-    console.log(response.data);
-    setState(response.data);
-   };
+    const todos = response.data;
+    todos.sort((a,b) => {
+      return a.id - b.id;
+    })
+    //console.log(response.data);
+    setState(todos);
+  };
+  
+  useEffect(() => {
    fetchTodos();
   }, [])
 
+  //create todos
   const addTodo = async () => {
     const response = await axios.post(HOST, { title: input });
     setState((state) => [...state, response.data]);
     setInput('');
   };
 
+  //event handler for submitting the todo you want to add
   const onSubmit = (e) => {
-    e.target.preventDefault();
+    e.preventDefault();
     addTodo();
   };
 
+  
+  //toggle event handler of checkbox completion of todo
+  const toggleComplete = async (todo) => {
+    const { id,completed } = todo;
+    await axios.put(HOST + id, { completed: !completed } );
+    await fetchTodos();
+  };
+
+  //delete todo event handler
+  const deleteTodo = async (id) => {
+    await axios.delete(HOST + id);
+    await fetchTodos();
+  };
+
+  //actual todos being displayed
   const todos = state.map(todo => {
     return (
-      <div>
-        {todo.title}
-        {' - ' + (todo.completed ? 'completed' : 'incomplete')}
+      <div key = {todo.id}>
+        <input 
+        type='checkbox' 
+        checked={todo.completed} 
+        onChange={() => toggleComplete(todo)}
+        />
+        {todo.title} {' '} 
+        <button onClick={() => deleteTodo(todo.id)}> DELETE </button>
       </div>
     );
   });
 
+  //form for entering todos
   return (
     <>
       <form onSubmit={onSubmit}>
